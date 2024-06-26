1e70e43e75f58b9ce61b2532c3f715d3a73ecd03
Cristian Hernandez <chinorubikguitar@hotmail.com>
2024-06-25 20:19:36 -0600

    test(whiteblox-vs-blackbox):

--- src/components/HelloWorld.spec.ts
+++ src/components/HelloWorld.spec.ts
@@ -14,3 +14,25 @@ it("Should render the msg property", () => {
     expect(instance.find('h1').text()).toBe('My first test')
     console.log(instance.html())
 });
+
+
+// This use whitebox, difficult to maintain, because we are using methods
+it("Should incremment count when the increment method is called", () => {
+    const instance = mount(HelloWorld);
+
+    instance.vm.increment()
+    expect(instance.vm.count).toBe(1)
+})
+
+
+// This use blackbox, better choice to test, because we are testing inputs and outputs. This case
+// input is the buttom, and the output is the final result to be expected
+
+it('Should increment the count and display it', async () => {
+    const instance = mount(HelloWorld);
+
+    const button = instance.find('button');
+    await button.trigger('click');
+
+    expect(button.text()).toBe('count is 1')
+})

986a29a27b47be4dfbe15058ea98cc0f8dc3ed1c
Cristian Hernandez <chinorubikguitar@hotmail.com>
2024-07-01 09:08:24 -0600

    child evento to binding with child

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import TitleComponent from '../components/TitleComponent.vue';
 import { useAppStore } from '../stores/appStore.ts';
+import { ref } from 'vue';
 
+const value = ref('About')
 const executeChangeMessage = () => {
-    useAppStore().changeMessage('testis')
+  useAppStore().changeMessage('testis')
 }
 </script>
 <template>
-    <title-component value="About" @on-mounted="executeChangeMessage"></title-component>
+  <title-component :value="value" @on-mounted="executeChangeMessage" @click="value = 'About !!!'"></title-component>
 </template>
--- src/views/about.spec.ts
+++ src/views/about.spec.ts
@@ -1,21 +1,28 @@
-import { describe, expect, it, vi } from "vitest";
+import { describe, expect, it, vi, beforeEach } from "vitest";
 import wrapperFactory from "../utils/wrapperFactory";
 import AboutView from "./AboutView.vue";
 import TitleComponent from "../components/TitleComponent.vue";
 import { useAppStore } from "../stores/appStore";
 
+let wrapper = wrapperFactory(AboutView);
 
 describe('Home suites tests', () => {
+
+    beforeEach(() => {
+        wrapper = wrapperFactory(AboutView);
+    })
     it('should dispatch changeMessage when titleComponent emits on-mounted', () => {{
-        const wrapper = wrapperFactory(AboutView)
-
         const titleComponenetWrapper = wrapper.findComponent(TitleComponent)
-
         const store = useAppStore()
-
         titleComponenetWrapper.vm.$emit('on-mounted', 'testis')
-
         expect(store.changeMessage).toHaveBeenCalledWith('testis')
     }})
 
+    it("should change the title whente titleComponent emit click event", async () => {
+        const titleComponenetWrapper = wrapper.findComponent(TitleComponent)
+        
+        await titleComponenetWrapper.trigger('click')
+
+        expect(titleComponenetWrapper.props('value')).toBe('About !!!')
+    })
 })

ce5aa8caaacb4ee86d2ab88258a03513e1de3f07
Cristian Hernandez <chinorubikguitar@hotmail.com>
2024-07-01 18:39:55 -0600

    getter from stroe to event emission

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,13 +1,28 @@
 <script setup lang="ts">
 import TitleComponent from '../components/TitleComponent.vue';
-import { useAppStore } from '../stores/appStore.ts';
-import { ref } from 'vue';
+import { useAppStore } from './../stores/appStore.ts';
+import { ref, watch } from 'vue';
+import { storeToRefs } from 'pinia';
 
-const value = ref('About')
+const value = ref<string>('About')
+const display_title = ref<boolean>(false)
+const { getAllMessage } = storeToRefs(useAppStore())
+const emit = defineEmits<{
+  (e: 'new-message', value: string): void
+}>()
+
 const executeChangeMessage = () => {
   useAppStore().changeMessage('testis')
 }
+
+watch(getAllMessage, (value) => {
+  if (!value) return
+
+  emit('new-message', value)
+})
 </script>
 <template>
-  <title-component :value="value" @on-mounted="executeChangeMessage" @click="value = 'About !!!'"></title-component>
+  <title-component :value="value" @on-mounted="executeChangeMessage" @click="value = 'About !!!'" v-if="display_title"
+  ></title-component>
+  <button @click="display_title = !display_title">Toggle title</button>
 </template>
--- src/views/about.spec.ts
+++ src/views/about.spec.ts
@@ -2,27 +2,49 @@ import { describe, expect, it, vi, beforeEach } from "vitest";
 import wrapperFactory from "../utils/wrapperFactory";
 import AboutView from "./AboutView.vue";
 import TitleComponent from "../components/TitleComponent.vue";
-import { useAppStore } from "../stores/appStore";
+import { useAppStore } from './../stores/appStore.ts';
 
-let wrapper = wrapperFactory(AboutView);
+let wrapper;
 
 describe('Home suites tests', () => {
 
     beforeEach(() => {
         wrapper = wrapperFactory(AboutView);
     })
-    it('should dispatch changeMessage when titleComponent emits on-mounted', () => {{
-        const titleComponenetWrapper = wrapper.findComponent(TitleComponent)
+    // it('should dispatch changeMessage when titleComponent emits on-mounted', () => {{
+    //     const titleComponenetWrapper = wrapper.findComponent(TitleComponent)
+    //     const store = useAppStore()
+    //     titleComponenetWrapper.vm.$emit('on-mounted', 'testis')
+    //     expect(store.changeMessage).toHaveBeenCalledWith('testis')
+    // }})
+
+    // it("should change the title whente titleComponent emit click event", async () => {
+    //     const titleComponenetWrapper = wrapper.findComponent(TitleComponent)
+    //     await titleComponenetWrapper.trigger('click')
+    //     expect(titleComponenetWrapper.props('value')).toBe('About !!!')
+    // })
+
+    // it("should toggle TitleComponent when the button is clicked", async () => {
+    //   const button = wrapper.find('button') 
+    //   let titleComponenetWrapper = wrapper.findComponent(TitleComponent)
+
+    //   await button.trigger('click')
+    //   expect(titleComponenetWrapper.exists()).toBe(false)
+
+   
+    //   await button.trigger('click')
+    //   titleComponenetWrapper = wrapper.findComponent(TitleComponent)
+    //   expect(titleComponenetWrapper.exists()).toBe(false)
+
+    // })
+
+    it("should emmit new message event when myMessage changes", async () => {
         const store = useAppStore()
-        titleComponenetWrapper.vm.$emit('on-mounted', 'testis')
-        expect(store.changeMessage).toHaveBeenCalledWith('testis')
-    }})
+        store.myMessage = 'new message'
 
-    it("should change the title whente titleComponent emit click event", async () => {
-        const titleComponenetWrapper = wrapper.findComponent(TitleComponent)
-        
-        await titleComponenetWrapper.trigger('click')
-
-        expect(titleComponenetWrapper.props('value')).toBe('About !!!')
+        await wrapper.vm.$nextTick()
+        expect(wrapper.emitted('new-message')).toBeTruthy()
+        expect(wrapper.emitted('new-message')).toHaveLength(1)
+        expect(wrapper.emitted('new-message')?.[0][0]).toBe('Complete message new message')
     })
 })
